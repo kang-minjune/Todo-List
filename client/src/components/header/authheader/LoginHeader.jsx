0162c586c3460915a4e6a2d1ae203e8da680836d
kang-minjune <bkokmj0327@gmail.com>
2024-09-26 03:04:07 +0900

    axios 모듈 설치, 로그인 기능 클라이언트 연동, 로그인 헤더 생성

---
+++ client/src/components/header/authheader/LoginHeader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../authheader/login-header.scss'
+
+const LoginHeader = () => {
+    return (
+        <div className='login-header'>
+            <span>
+                LOGIN
+            </span>
+        </div>
+    );
+};
+
+export default LoginHeader;
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,76 +1,117 @@
 import React, { useState, useContext } from "react";
+import axios from 'axios';
+import { AuthContext } from '../context/AuthContext';
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import AuthBtn from '../components/auth/AuthBtn';
-import AuthCheckbox from '../components/auth/AuthCheckbox';
-import AuthInput from '../components/auth/AuthInput';
+import AuthInput from '../components/auth/AuthInput'
+import AuthCheckbox from '../components/auth/AuthCheckbox'
 
-import { NavLink } from 'react-router-dom';
+import LoginHeader from '../components/header/authheader/LoginHeader';
+
 import '../styles/login.scss';
 
 /**
  * 로그인 페이지 
  * @returns
  */
+
 const Login = () => {
 
+  const [Authlogin, setAuthlogin] = useState({
+     email: undefined,
+     password: undefined,
+  });
+  
+  const navigate = useNavigate();
+
+  const { loading, error, dispatch } = useContext(AuthContext);
+
+  const loginClickHandler = async (e) => {
+    try {
+      const apiUrl = process.env.REACT_APP_API_URL;
+      console.log(`API URL: ${apiUrl}`)
+      const response = await axios.post(`${apiUrl}/auth/login`, Authlogin, { withCredentials: true });
+      console.log(response.data);
+      console.log("안녕하세요.")
+      // dispatch({ type: "LOGIN", payload: response.data.user });
+      alert(response.data.message);
+      navigate('/');
+    } catch (err) {
+      if (err.response && err.response.data && err.response.data.message) {
+        alert(err.response.data.message);
+      } else {
+        alert("로그인 정보 잘못되었습니다. 다시 시도해주세요.")
+      }
+    }
+  };
+
+  const loginHandleChange = (e) => {
+    setAuthlogin((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+ 
   return (
-    <div className="login-form">
-      <div className="login-container">
-        {/* 로그인 메인 폼 */}
-        <div className="login-contents">
-          <div className="login-logo-box">
-            <NavLink to={'/'} className='login-home-logo'>
-                 <span className="login-youth"><img src='./QuickListLogo.png' width="110px" height="80px" /></span>
-            </NavLink>
+    <div className='login-header-contents'>
+      <LoginHeader/>
+        <div className="login-form">
+          <div className="login-container">
+            {/* 로그인 메인 폼 */}
+            <div className="login-contents">
+              <div className="login-logo-box">
+                <NavLink to={'/'} className='login-home-logo'>
+                    <span className="login-youth"><img src='./QuickListLogo.png' width="110px" height="80px" /></span>
+                </NavLink>
+              </div>
+
+              <div className="login-input-form">
+                <AuthInput
+                  formClassName="input-id-bar"
+                  imgSrc={"./icons/idlogo.png"}
+                  imgAlt="로그인 아이콘"
+                  inputType="text"
+                  inputClassName="login-id-input"
+                  inputPlaceHolder="아이디"
+                  id={"userId"}
+                  inputOnChange={loginHandleChange}
+                />
+
+
+                <AuthInput
+                  formClassName="input-pw-bar"
+                  imgSrc={"./icons/pwlogo.png"}
+                  imgAlt="로그인 아이콘"
+                  inputType="password"
+                  inputClassName="login-pw-input"
+                  inputPlaceHolder="비밀번호"
+                  inputOnChange={loginHandleChange}
+                  id={"password"}
+                  authOnKeyDown={(e)=>(e.key === "Enter" ) ? loginClickHandler() : ""}
+                />
+              </div>
+
+              <div className="login-maintain-container">
+                <AuthCheckbox inputType="checkbox" checkBoxText="로그인 상태 유지" />
+              </div>
+
+              <AuthBtn
+                btnClassName="login-btn"
+                btnText="로그인"
+                btnOnClick={loginClickHandler}
+              />
+
+              <div className="login-userfind-register">
+
+                <NavLink className={'find-id'} to={'/userfind'} >아이디 찾기</NavLink>
+                <NavLink className={'find-pw'} to={'/userfind'}>비밀번호 찾기</NavLink>
+                <NavLink className={'go-register'} to={'/register'}>회원가입</NavLink>
+
+              </div>
+
+            
+              </div>
+            </div>
           </div>
-
-          <div className="login-input-form">
-            <AuthInput
-              formClassName="input-id-bar"
-              imgSrc={"./icons/idlogo.png"}
-              imgAlt="로그인 아이콘"
-              inputType="text"
-              inputClassName="login-id-input"
-              inputPlaceHolder="아이디"
-              id={"userId"}
-            //   inputOnChange={handleChange}
-            />
-
-
-            <AuthInput
-              formClassName="input-pw-bar"
-              imgSrc={"./icons/pwlogo.png"}
-              imgAlt="로그인 아이콘"
-              inputType="password"
-              inputClassName="login-pw-input"
-              inputPlaceHolder="비밀번호"
-            //   inputOnChange={handleChange}
-              id={"password"}
-            //   authOnKeyDown={(e)=>(e.key === "Enter" ) ? loginBtnClickHandler() : ""}
-            />
-          </div>
-
-          <div className="login-maintain-container">
-            <AuthCheckbox inputType="checkbox" checkBoxText="로그인 상태 유지" />
-          </div>
-
-          <AuthBtn
-            btnClassName="login-btn"
-            btnText="로그인"
-            // btnOnClick={loginBtnClickHandler}
-          />
-
-          <div className="login-userfind-register">
-
-            <NavLink className={'find-id'} to={'/userfind'} >아이디 찾기</NavLink>
-            <NavLink className={'find-pw'} to={'/userfind'}>비밀번호 찾기</NavLink>
-            <NavLink className={'go-register'} to={'/register'}>회원가입</NavLink>
-
-          </div>
-
-         
-          </div>
-        </div>
       </div>
   );
 };
