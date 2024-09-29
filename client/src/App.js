3e52d0be666bb6babb300ec2393089d0d5bf9704
kang-minjune <bkokmj0327@gmail.com>
2024-09-30 07:59:47 +0900

    로그인페이지 경로 수정

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
          <Routes>
-              <Route path='/login' element={<Login />} />
+              <Route path='/' element={<Login />} />
               <Route path='/list' element={<List />} />
               <Route path='/calendar' element={<Calendar />} />
               <Route path='/mypage' element={<Mypage />} />
