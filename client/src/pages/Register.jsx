ce7ed4f8063896548843f29519b98e352724c432
kang-minjune <bkokmj0327@gmail.com>
2024-10-05 00:30:25 +0900

    회원가입 폼 세로값 수정

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,10 +3,10 @@ import Header from '../components/header/Header';
 import RegisterFooter from '../components/footer/authfooter/RegisterFooter';
 import AddressSearchBtn from '../components/auth/AddressSearchBtn';
 import { useNavigate } from 'react-router-dom';
-
+import axios from 'axios';
 
 import '../styles/register.scss'
-import axios from 'axios';
+
 
 const Register = () => {
     const navigate = useNavigate();
@@ -79,97 +79,101 @@ const Register = () => {
 
 
     return (
-        <div className='register-page'>
+        <div>
             <Header />
+                <div className='register-page'>
+                    
 
-            <div className='container'>
-                <label className='user-id'>아이디</label>
-                <input id='username' type='text' onChange={registerHandleChange} placeholder='사용할 아이디를 작성해주세요.' />
+                    <div className='container'>
+                        <label className='user-id'>아이디</label>
+                        <input id='username' type='text' onChange={registerHandleChange} placeholder='사용할 아이디를 작성해주세요.' />
 
-                <label>이름</label>
-                <input id='realname' type='text' onChange={registerHandleChange} placeholder='이름을 작성해주세요.' />
+                        <label>이름</label>
+                        <input id='realname' type='text' onChange={registerHandleChange} placeholder='이름을 작성해주세요.' />
 
 
-                <label>패스워드</label>
-                <div className='password-div'>
-                     <input
-                        id='password'
-                        type={showPassword ? 'password' : 'text'}
-                        onChange={registerHandleChange}
-                        placeholder='패스워드를 작성해주세요.'
-                    />
+                        <label>패스워드</label>
+                        <div className='password-div'>
+                            <input
+                                id='password'
+                                type={showPassword ? 'password' : 'text'}
+                                onChange={registerHandleChange}
+                                placeholder='패스워드를 작성해주세요.'
+                            />
 
-                    <button onClick={showPasswordHandler} className='eyebtn'>
-                        {showPassword ? (
-                            <img src="./icons/eyeoff.png" alt="패스워드 보기 off" />
-                        ) : (
-                            <img src="./icons/eyeon.png" alt="패스워드 보기 on" />
-                        )}
-                    </button>
+                            <button onClick={showPasswordHandler} className='eyebtn'>
+                                {showPassword ? (
+                                    <img src="./icons/eyeoff.png" alt="패스워드 보기 off" />
+                                ) : (
+                                    <img src="./icons/eyeon.png" alt="패스워드 보기 on" />
+                                )}
+                            </button>
+                        </div>
+                    
+
+                        <label>패스워드 확인</label>
+                        <div className='password-div'>
+                            <input 
+                                id='passwordconfirm' 
+                                type={showPasswordConfirm ? 'password' : 'text'} 
+                                onChange={registerHandleChange} 
+                                placeholder='패스워드를 동일하게 작성해주세요.' />
+
+                            <button onClick={showPasswordConfirmHandler} className='eyebtn'>
+                                    {showPasswordConfirm ? (
+                                        <img src="./icons/eyeoff.png" alt="패스워드 보기 off" />
+                                    ) : (
+                                        <img src="./icons/eyeon.png" alt="패스워드 보기 on" />
+                                    )}
+                            </button>
+                        </div>
+                        
+
+                        <div className='button'>
+                            <span>성별</span>
+                            <button
+                                className={`man ${credentials.gender === "남성" ? "selected" : ""}`}
+                                onClick={() => handleGenderClick("남성")}
+                            >
+                                남성
+                            </button>
+                            <button
+                                className={`woman ${credentials.gender === "여성" ? "selected" : ""}`}
+                                onClick={() => handleGenderClick("여성")}
+                            >
+                                여성
+                            </button>
+                        </div>
+
+                        <label>E-Mail</label>
+                        <input id='email' type='email' onChange={registerHandleChange} placeholder='이메일을 작성해주세요.' />
+
+                        <label>전화번호</label>
+                        <input id='phone' type='tel' onChange={registerHandleChange} placeholder='하이픈(-)을 포함해주세요' />
+                        
+                        <label>주소</label>
+
+                        <input id='address' type='text' value={credentials.address} onChange={registerHandleChange} placeholder='도로명 주소를 작성해주세요.' />
+                        <AddressSearchBtn onAddressSelect={handleAddressSelect} />
+
+                        
+                        <label className='address-detail'>상세주소</label>
+                        <input id='addressdetail' type='text' onChange={registerHandleChange} placeholder='상세 주소를 작성해주세요.' />
+
+                        <button className='register-button' onClick={registerBtnClickHandler}>
+                            가입하기
+                        </button>
+
+                        <button className='login-nav' onClick={LoginNav}>
+                            로그인 이동
+                        </button>
+                    </div>
+                    
+                    
+
                 </div>
-               
-
-                <label>패스워드 확인</label>
-                <div className='password-div'>
-                    <input 
-                           id='passwordconfirm' 
-                           type={showPasswordConfirm ? 'password' : 'text'} 
-                           onChange={registerHandleChange} 
-                           placeholder='패스워드를 동일하게 작성해주세요.' />
-
-                    <button onClick={showPasswordConfirmHandler} className='eyebtn'>
-                            {showPasswordConfirm ? (
-                                <img src="./icons/eyeoff.png" alt="패스워드 보기 off" />
-                            ) : (
-                                <img src="./icons/eyeon.png" alt="패스워드 보기 on" />
-                            )}
-                    </button>
-                </div>
-                
-
-                <div className='button'>
-                    <span>성별</span>
-                    <button
-                        className={`man ${credentials.gender === "남성" ? "selected" : ""}`}
-                        onClick={() => handleGenderClick("남성")}
-                    >
-                        남성
-                    </button>
-                    <button
-                        className={`woman ${credentials.gender === "여성" ? "selected" : ""}`}
-                        onClick={() => handleGenderClick("여성")}
-                    >
-                        여성
-                    </button>
-                </div>
-
-                <label>E-Mail</label>
-                <input id='email' type='email' onChange={registerHandleChange} placeholder='이메일을 작성해주세요.' />
-
-                <label>전화번호</label>
-                <input id='phone' type='tel' onChange={registerHandleChange} placeholder='하이픈(-)을 포함해주세요' />
-                
-                <label>주소</label>
-
-                <input id='address' type='text' value={credentials.address} onChange={registerHandleChange} placeholder='도로명 주소를 작성해주세요.' />
-                <AddressSearchBtn onAddressSelect={handleAddressSelect} />
-
-                
-                <label className='address-detail'>상세주소</label>
-                <input id='addressdetail' type='text' onChange={registerHandleChange} placeholder='상세 주소를 작성해주세요.' />
-
-                <button className='register-button' onClick={registerBtnClickHandler}>
-                    가입하기
-                </button>
-
-                <button className='login-nav' onClick={LoginNav}>
-                    로그인 이동
-                </button>
+                <RegisterFooter />
             </div>
-            
-            <RegisterFooter />
-
-        </div>
     );
 };
 
